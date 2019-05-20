170c22185a6161bf6f791b53796b7eac65047f4f
Alex Johnston <alexjohnston@ymail.com>
2019-05-20 10:43:54 +0100

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,40 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@artists = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@artists << self
+  end
+
+  def self.all
+    @@artists
+  end
+
+  def paintings
+    Painting.all.select {|painting| painting.artist == self}
+  end
+
+  def galleries
+    paintings.map {|painting| painting.gallery}
+  end
+
+  def cities
+    galleries.map {|galleries| galleries.city}
+  end
+
+  def self.total_experience
+    @@artists.reduce(0){|total, artists| total + artists.years_experience}
+  end
+
+  def self.most_prolific
+    @@artists.max_by{|artist| artist.years_experience/artist.paintings.size}
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, gallery, self)
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,14 @@
 require_relative '../config/environment.rb'
 
-
+sam = Artist.new("Sam", 100)
+catherine = Artist.new("Catherine", 80)
+alex = Artist.new("Alex", 135)
+moorgate = Gallery.new("Moorgate", "London")
+wimbledon = Gallery.new("Wimbledon", "San Francisco")
+sam.create_painting("the moors", 25, moorgate)
+sam.create_painting("where the wind blows", 35, wimbledon)
+alex.create_painting("discussion", 30, moorgate)
+catherine.create_painting("your ability is nigh", 35, moorgate)
 
 
 binding.pry
