9b0cf24a708ff9f67af14277bdb5f0763f234a72
flexkat <catherinemaritz1@gmail.com>
2019-05-20 13:12:26 +0100

    has many through relationships

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,40 @@
 class Artist
 
-  attr_reader :name, :years_experience
+  attr_reader :name, :years_experience, :painting
 
+  @@all = []
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select {|painting| painting.artist == self}
+  end
+
+  def galleries
+    paintings.map {|painting| painting.gallery}
+  end
+
+  def cities
+    galleries.map {|gallery| gallery.city}
+  end
+
+  def self.total_experience
+    @@all.reduce(0) {|sum, artist| sum + artist.years_experience}
+  end
+
+  def self.most_prolific
+    @@all.max_by{|artist| artist.years_experience.to_f / artist.paintings.size.to_f}
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
   end
 
 end
--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,16 @@ require 'pry'
 require_relative '../app/models/artist.rb'
 require_relative '../app/models/gallery.rb'
 require_relative '../app/models/painting.rb'
+
+louvre = Gallery.new("Louvre", "Paris")
+tate_mod = Gallery.new("Tate Modern", "London")
+cat = Artist.new("Cat", 13)
+sam = Artist.new("Sam", 2)
+alex = Artist.new("Alex", 7)
+
+alex.create_painting("Sunshine", 150, tate_mod)
+cat.create_painting("Blank Spaces", 400, tate_mod)
+sam.create_painting("How did I get here", 300, tate_mod)
+cat.create_painting("Dark Spaces", 550, louvre)
+
+binding.pry
