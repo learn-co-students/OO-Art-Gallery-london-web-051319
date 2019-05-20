d431510fec30d3adee28ab05b5272de74d6cf655
Jowita <jowita.lapies@gmail.com>
2019-05-20 17:42:40 +0100

    Art Gallery with Maija

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,52 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
+
+ def self.all
+   @@all
+ end
+
+ def paintings
+   Painting.all.select do |painting|
+     painting.artist == self
+   end
+ end
+
+ def galleries
+   paintings.map do |painting|
+     painting.gallery
+   end.uniq
+ end
+
+ def cities
+   galleries.map do |gallery|
+     gallery.city
+   end.uniq
+ end
+
+ def self.total_experience
+   all.map do |artist|
+     artist.years_experience
+   end.reduce(:+)
+ end
+
+ def self.most_prolific
+   all.max_by do |artist|
+     artist.paintings.length / artist.years_experience.to_f
+  end
+end
+
+def create_painting(title, price, gallery)
+  Painting.new(title, price, gallery, self)
+end
+
+
 
 end
