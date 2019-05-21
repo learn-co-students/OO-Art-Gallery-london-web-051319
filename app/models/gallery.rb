553daf0b9b95f42fd60e03c283c8ab1dfcb05b0d
github username <github email address>
2019-05-21 08:58:36 +0100

    Completed class relations

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,44 @@
 class Gallery
 
-  attr_reader :name, :city
+  attr_reader :name, :city 
 
-  def initialize(name, city)
-    @name = name
-    @city = city
-  end
+#  CLASS ******************
+
+@@all = []
+
+def self.all
+   @@all
+end
+
+def initialize(name: , city: )
+   @name = name
+   @city = city
+   @@all << self
+end
+
+# def self.most_expensive_painting 
+#    a = Painting.all.map.with_index{|inst|inst.price}.max_by{|x| x}
+#    # b = a.max_by{|inst| inst} # gives max value in array a
+#    Painting.all.select{|inst|inst.price == a}
+# end
+
+#  INSTANCE ******************
+
+def paintings
+Painting.all.select{|inst| inst.gallery == self}
+end
+
+def artists
+   paintings.map{|inst| inst.artist}
+end
+
+def artist_names
+   artists.map{|inst| inst.name}
+end
+
+def most_expensive_painting
+   prices = paintings.map{|inst| inst.price}.max_by{|x| x}
+   paintings.select{|inst| inst.price == prices}
+end
 
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,28 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :artist, :gallery
+
+  #  CLASS ******************
 
   @@all = []
+  
+  def self.all
+   @@all
+end
 
-  def initialize(title, price)
-    @title = title
-    @price = price
-  end
+def initialize(title: , price: , artist: , gallery: )
+   @title = title
+   @price = price
+   @artist = artist
+   @gallery = gallery
+   @@all << self
+end
+
+def self.total_price # in millions
+   Painting.all.map{|inst| inst.price}.inject(0){|sum,x| sum+x}.to_i
+end
+
+#  INSTANCE ******************
+
 
 end
